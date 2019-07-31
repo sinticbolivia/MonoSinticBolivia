c77c0df87e7082a11f28569663e79cc20d15903a
J. Marcelo Aviles Paco <maviles@sinticbolivia.net>
2019-07-30 22:06:34 -0400

    Added main solution

---
+++ SinticBolivia/Agenda/agenda.cs
@@ -0,0 +1,147 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Collections;
+using System.Linq;
+using System.Text;
+using SmartBiz;
+using SmartBiz.Database;
+
+namespace SmartBiz.Agenda
+{
+    public class SMAgenda : SMObject
+    {
+        public int id;
+        public int id_objeto;
+		public int id_tipo_objeto;
+        public string descripcion;
+        public DateTime fecha_inicio;
+        public DateTime fecha_termino;
+        public DateTime fecha_creacion;
+		
+		protected SMObjetoAgenda obj;
+		
+        public SMAgenda()
+        {
+ 
+        }
+		public SMAgenda(int id)
+		{
+			this.obtenerInformacionPorID(id);
+		}
+        public int agendarObjeto(SMObjetoAgenda obj)
+        {
+			this.id = Int32.Parse(SMFactory.getDbh().getTableCode("agenda", "id").ToString());
+			//obtener un id para la nueva instance del objeto a agendar
+			this.id_objeto = Int32.Parse(SMFactory.getDbh().getTableCode("agenda", "id_objeto").ToString());
+			//asignar el tipo de objeto
+			this.id_tipo_objeto = obj.id_tipo_objeto;
+			this.fecha_creacion = DateTime.Now;
+			int id_entrada = SMFactory.getDbh().insert("agenda", this);
+			//almacenar las propiedades del objeto
+			ArrayList propiedades = obj.obtenerPropiedades();
+			if( propiedades != null )
+			{
+				foreach(Hashtable key in propiedades)
+				{
+                    Hashtable p = new Hashtable();
+                    p.Add("id_propiedad", SMFactory.getDbh().getTableCode("agenda_objeto_propiedades", "id_propiedad"));
+                    p.Add("id_objeto", this.id_objeto);
+                    p.Add("propiedad", key["propiedad"]);
+
+                    p.Add("valor", key["valor"]);
+                    p.Add("fecha_creacion", DateTime.Now.ToString("yyyyMMdd HH:mm:ss"));
+                    SMFactory.getDbh().insert("agenda_objeto_propiedades", p);
+					
+				}
+			}
+			
+			return id_entrada;
+        }
+        public bool obtenerInformacionPorID(int id)
+        {
+            string query = "SELECT " + this.getPropertiesWithGlue(",") +
+                            " FROM agenda " +
+                            "WHERE id = " + id.ToString();
+            Hashtable row = SMFactory.getDbh().QueryRow(query);
+            if (row == null)
+                throw new Exception("Objecto de agenda no encontrado");
+			/*
+            this.id_objeto = Int32.Parse(row["id_objeto"].ToString());
+            this.descripcion = row["descripcion"].ToString();
+            this.fecha_inicio = DateTime.Parse(row["fecha_inicio"].ToString());
+            this.fecha_termino = DateTime.Parse(row["fecha_termino"].ToString());
+            */
+			this.bind(row);
+			//obtener el objeto asociado a la agenda
+			//this.obj = new SMObjetoAgenda(this.id_objeto);
+			
+            return true;
+        }
+		public static SMAgenda obtenerEntradaPorIDObjecto(int id_objeto)
+		{
+			SMAgenda entrada = new SMAgenda();
+			string query = "SELECT " + entrada.getPropertiesWithGlue(",") +
+                            " FROM agenda " +
+                            "WHERE id_objeto = " + id_objeto.ToString();
+			Hashtable row = SMFactory.getDbh().QueryRow(query);
+            if (row == null)
+                throw new Exception("Objecto de agenda no encontrado");
+			
+			entrada.bind(row);
+			return entrada;
+		}
+		/// <summary>
+		/// Crear una nueva instance de un objeto de agenda
+		/// </summary>
+		/// <param name="tipo_objeto">
+		/// A <see cref="System.String"/>
+		/// </param>
+		/// <returns>
+		/// A <see cref="SMObjetoAgenda"/>
+		/// </returns>
+		public SMObjetoAgenda crearObjeto(string tipo_objeto)
+        {
+            SMObjetoAgenda obj = new SMObjetoAgenda();
+            string query = "SELECT id_tipo_objeto, tipo_objeto, fecha_creacion " +
+            				"FROM agenda_tipo_objetos "+
+                            " WHERE tipo_objeto = '"+tipo_objeto+"'";
+            Hashtable row = SMFactory.getDbh().QueryRow(query);
+			if( row != null )
+    	        obj.bind(row);
+            return obj;
+        }
+		/// <summary>
+		/// Obtiene el identificador de un tipo de objeto determinado 
+		/// </summary>
+		/// <param name="tipo_objeto">
+		/// A <see cref="System.String"/>
+		/// </param>
+		/// <returns>
+		/// A <see cref="System.Int32"/>
+		/// </returns>
+		public static int obtenerTipoObjetoID(string tipo_objeto)
+		{
+			string query = "SELECT id_tipo_objeto, tipo_objeto, fecha_creacion " +
+							"FROM agenda_tipo_objetos "+
+                            " WHERE tipo_objeto = '"+tipo_objeto+"'";
+            Hashtable row = SMFactory.getDbh().QueryRow(query);
+			if( row == null )
+    	        return 0;
+            return Int32.Parse(row["id_tipo_objeto"].ToString());
+		}
+		/// <summary>
+		/// Obtiene el objeto asociado a la entrada de la agenda 
+		/// </summary>
+		/// <returns>
+		/// A <see cref="SMObjetoAgenda"/>
+		/// </returns>
+		public SMObjetoAgenda obtenerObjeto()
+		{
+			return this.obj;
+		}
+		public void asignarObjeto(SMObjetoAgenda o)
+		{
+			this.obj = o;
+		}
+    }
+}
---
+++ SinticBolivia/Agenda/booking_object.cs
@@ -0,0 +1,188 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Collections;
+using System.Reflection;
+using System.Linq;
+using System.Text;
+using System.Data;
+using SmartBiz.Database;
+using System.Web.Script.Serialization;
+
+namespace SmartBiz.Agenda
+{
+    public class SMObjetoAgenda : SMObject
+    {
+        private int id_objeto;
+
+        public int id_tipo_objeto;
+        public string tipo_objeto;
+        public DateTime fecha_creacion;
+		
+		private ArrayList propiedades;
+		
+        public SMObjetoAgenda()
+        { 
+
+        }
+		public SMObjetoAgenda(int id_objeto)
+        { 
+			this.obtenerInformacion(id_objeto);
+        }
+        public bool obtenerInformacion(int id_objeto)
+        {
+			this.propiedades = new ArrayList();
+			
+            string query = "SELECT a.id_objeto, a.id_tipo_objeto, o.tipo_objeto " + 
+                            " FROM agenda a, agenda_tipo_objetos o "+
+                            "WHERE a.id_objeto = " + id_objeto.ToString() + " " +
+                            "AND a.id_tipo_objeto = o.id_tipo_objeto ";
+            Hashtable row = SMFactory.getDbh().QueryRow(query);
+            if (row == null)
+                throw new Exception("Objecto de agenda no encontrado");
+			
+            this.bind(row);
+			query = "SELECT id_propiedad, id_objeto, propiedad, valor, fecha_creacion " +
+						"FROM agenda_objeto_propiedades " +
+						"WHERE id_objeto = " + id_objeto.ToString();
+			this.propiedades = SMFactory.getDbh().Query(query);
+            /*
+			foreach(Hashtable r in list)
+			{
+				if(this.propiedades.ContainsKey(r["propiedad"].ToString()))
+				{
+					this.propiedades.Add(r["propiedad"].ToString() + "_" + r["valor"].ToString(), r["valor"].ToString());
+				}
+				else
+					this.propiedades.Add(r["propiedad"].ToString(), r["valor"].ToString());
+			}
+            */         
+            this.id_objeto = id_objeto;
+            return true;
+        }
+		public ArrayList obtenerPropiedades()
+		{
+			return this.propiedades;
+		}
+        public void asignarPropiedades(ArrayList propiedades)
+		{
+			this.propiedades = propiedades;
+		}
+        public bool actualizar()
+        {
+            string query = "UPDATE agenda SET ";
+            foreach(FieldInfo column in this.getProperties())
+            {
+                if (column.GetValue(this) is string )
+                {
+                    query += column.Name + " = '" + column.GetValue(this) + "',";
+                }
+                else if(column.GetValue(this) is int)
+                {
+                    query += column.Name + " = '" + column.GetValue(this) + "',";
+                }
+            }
+            query = query.Substring(query.Length, query.Length - 1);
+            query += " WHERE id = " + this.id_tipo_objeto;
+            SMFactory.getDbh().Execute(query);
+            return true;
+        }
+        public bool eliminarObjeto(int id)
+        {
+            string query = "DELETE FROM agenda WHERE id = " + id;
+            SMFactory.getDbh().Execute(query);
+            return true;
+        }
+        /// <summary>
+        /// Adiciona una nueva propiedad en la base de datos para un objeto de agenda
+        /// </summary>
+        /// <param name="nombre"></param>
+        /// <param name="valor"></param>
+        public int adicionarPropiedad(string nombre, string valor)
+        {
+            string query = "INSERT INTO agenda_objeto_propiedades(id_propiedad, id_objeto, propiedad, valor, fecha_creacion) "+
+                            "VALUES({0}, {1}, '{2}', '{3}', '{4}')";
+            query = String.Format(query, 
+                                    SMFactory.getDbh().getTableCode("agenda_objeto_propiedades", "id_propiedad"),
+                                    this.id_objeto,
+                                    nombre,
+                                    valor,
+                                    DateTime.Now.ToString("yyyyMMdd HH:mm:ss"));
+            return SMFactory.getDbh().Execute(query);
+        }
+        /// <summary>
+        /// Adiciona una nueva propiedad al objeto de agenda y serializa su valor en JSON
+        /// </summary>
+        /// <returns>
+        /// The propiedad.
+        /// </returns>
+        /// <param name='nombre'>
+        /// Nombre.
+        /// </param>
+        /// <param name='valor'>
+        /// Valor.
+        /// </param>
+        public int adicionarPropiedad(string nombre, Hashtable data)
+        {
+            JavaScriptSerializer ser = new JavaScriptSerializer();
+            string json = ser.Serialize(data);
+            return this.adicionarPropiedad(nombre, json);
+        }
+        /// <summary>
+        /// Actualiza la propieda del objeto de agenda
+        /// </summary>
+        public void actualizarPropiedad(int id_propiedad, string propiedad, string nuevo_valor)
+        {
+            Hashtable data = new Hashtable();
+            data.Add(propiedad, nuevo_valor);
+            Hashtable w = new Hashtable();
+            w.Add("id_propiedad", id_propiedad);
+            SMFactory.getDbh().update("agenda_objeto_propiedades", data, w);
+        }
+        public void actualizarPropiedad(int id_propiedad, string propiedad, Hashtable data)
+        {
+            JavaScriptSerializer ser = new JavaScriptSerializer();
+            string json = ser.Serialize(data);
+            this.actualizarPropiedad(id_propiedad, propiedad, json);
+        }
+        public object obtenerPropiedad(string propiedad)
+        {
+            object result = null;
+
+            foreach(Hashtable p in this.propiedades)
+            {
+                if( !p["propiedad"].ToString().Equals(propiedad) )
+                    continue;
+                if( (p["valor"].ToString().StartsWith("{") && p["valor"].ToString().EndsWith("}"))
+                     || (p["valor"].ToString().StartsWith("[") && p["valor"].ToString().EndsWith("]"))  )
+                {
+                    JavaScriptSerializer ser = new JavaScriptSerializer();
+                    result = ser.DeserializeObject(p["valor"].ToString());
+                }
+                else
+                {
+                    result = p["valor"];
+                }
+                break;
+            }
+            return result;
+        }
+        /// <summary>
+        /// Obtiene todas las propiedades de un objeto con un determinado nombre
+        /// </summary>
+        /// <returns>
+        /// The propiedades.
+        /// </returns>
+        public ArrayList buscarPropiedades(string propiedad)
+        {
+            ArrayList result = new ArrayList();
+            foreach(Hashtable p in this.propiedades)
+            {
+                if( p["propiedad"].ToString().Equals(propiedad) )
+                {
+                    result.Add(p);
+                }
+            }
+            return result;
+        }
+    }
+}
